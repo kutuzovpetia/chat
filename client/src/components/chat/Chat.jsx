d5f189911b6e7e0f22f8debf0a340f64be900a24
kutuzovpetia <petiakutuzov@gmail.com>
2021-12-14 12:03:37 +0200

    +header

--- client/src/components/chat/Chat.jsx
+++ client/src/components/chat/Chat.jsx
@@ -1,18 +1,31 @@
 import s from './chat-style.module.sass';
+import arrow from '../../img/arrow-blue.svg';
+import camera from '../../img/camera.svg';
 
 const Chat = () =>{
+
     return(
         <>
             <div className={s.header}>
-                <button>
-                    <img src="" alt="arrow"/>
-                </button>
+                <div className={s.headerControls}>
+                    <button>
+                        <img src={arrow} alt="arrow"/>
+                    </button>
 
-                <div className={s.avatar}>
-                    <img src="https://avochka.ru/img/kartinka/1/enot_glass.jpg" alt="avatar"/>
+                    <div className={s.avatar}>
+                        <img src="https://avochka.ru/img/kartinka/1/enot_glass.jpg" alt="avatar"/>
+                    </div>
+
+                    <button>
+                        <img src={camera} alt="arrow"/>
+                    </button>
                 </div>
 
-                <a href="/">камера</a>
+                <h5 className={s.userName}>Hell Boy</h5>
+            </div>
+
+            <div>
+                <h1>Content</h1>
             </div>
         </>
     )
