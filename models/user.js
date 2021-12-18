701902bf5ea87e69b4e277787b974b1d52f66826
kutuzovpetia <petiakutuzov@gmail.com>
2021-12-18 11:06:25 +0200

    Remade userSchema

--- models/user.js
+++ models/user.js
@@ -1,10 +1,11 @@
 const {Schema, model} = require('mongoose');
 
 const userSchema = new Schema({
-    name: { type: String, required: true },
-    password: { type: String, required: true },
-    details: { type: String },
-    imgUrl: { type: String }
+    firstName: { type: String, required: true },
+    secondName: { type: String, required: true },
+    details: { type: String, required: true },
+    phoneOrEmail: { type: String, required: true },
+    password: {type: String, required: true}
 });
 
 module.exports = model('User', userSchema);
--- routes/registration-route.js
+++ routes/registration-route.js
@@ -28,32 +28,36 @@ router.get('/', (req,res)=>{
 
 router.post('/', fileMiddleware.single('photo'), async (req, res)=>{
 
-    const {name, password, details, imgUrl} = req.body;
-    const candidate = await User.findOne({name});
+    console.log(req.body)
 
-    try {
-        if(candidate){
-            fs.remove(path.join(__dirname, `/../uploads/${req.file.filename}`))
-            res.status(401).send({message: 'User with this name exists'});
-        }else {
-            const hashPassword = await bcrypt.hash(password, 10);
-            const user = new User({name, password: hashPassword});
-            const folderName = user.id;
-
-            fs.mkdirs(path.join(__dirname, `/../uploads/${folderName}`));
-            const src = path.join(__dirname, `/../${req.file.path}`);
-            const dest = path.join(__dirname, `/../uploads/${folderName}/${req.file.filename}`);
-
-            fs.move(src, dest, { overwrite: true }, (err)=>{ if (err) return console.error(err)});
-            user.imgUrl = `${folderName}/${req.file.filename}`;
-
-            await user.save(()=>{
-                res.send(user);
-            });
-        }
-    }catch (err){
-        throw err
-    }
+    // const {firstName, secondName, details, password, phoneOrEmail, file} = req.body;
+    //
+    //
+    // const candidate = await User.findOne({firstName});
+    //
+    // try {
+    //     if(candidate){
+    //         fs.remove(path.join(__dirname, `/../uploads/${req.file.filename}`))
+    //         res.status(401).send({message: 'User with this name exists'});
+    //     }else {
+    //         const hashPassword = await bcrypt.hash(password, 10);
+    //         const user = new User({firstName, secondName, details, phoneOrEmail, password: hashPassword});
+    //         const folderName = user.id;
+    //
+    //         fs.mkdirs(path.join(__dirname, `/../uploads/${folderName}`));
+    //         const src = path.join(__dirname, `/../${req.file.path}`);
+    //         const dest = path.join(__dirname, `/../uploads/${folderName}/${req.file.filename}`);
+    //
+    //         fs.move(src, dest, { overwrite: true }, (err)=>{ if (err) return console.error(err)});
+    //         user.imgUrl = `${folderName}/${req.file.filename}`;
+    //
+    //         await user.save(()=>{
+    //             res.send(user);
+    //         });
+    //     }
+    // }catch (err){
+    //     throw err
+    // }
 });
 
 module.exports = router;
